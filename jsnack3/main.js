202561586a7217cc92d13d1a064cd55b5c6350ad
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-26 11:54:42 +0200

    Corretto con ciclo for

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,26 +1,11 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-const num1 = Number( prompt ("Inserisci un numero") );
-console.log(num1);
-const num2 = Number( prompt ("Inserisci un numero") );
-console.log(num2);
-const num3 = Number( prompt ("Inserisci un numero") );
-console.log(num3);
-const num4 = Number( prompt ("Inserisci un numero") );
-console.log(num4);
-const num5 = Number( prompt ("Inserisci un numero") );
-console.log(num5);
-const num6 = Number( prompt ("Inserisci un numero") );
-console.log(num6);
-const num7 = Number( prompt ("Inserisci un numero") );
-console.log(num7);
-const num8 = Number( prompt ("Inserisci un numero") );
-console.log(num8);
-const num9 = Number( prompt ("Inserisci un numero") );
-console.log(num9);
-const num10 = Number( prompt ("Inserisci un numero") );
-console.log(num10);
+let sum = 0;
 
-const sumNumbers = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
-console.log("La somma è:", sumNumbers);
+for( let i = 0; i < 10; i++) {
+    const number = Number( prompt("Inserisci un numero") );
+    sum = sum + number;
+}
+
+console.log("La somma è:", sum);
