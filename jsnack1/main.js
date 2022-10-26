ea2f63b320f2309e956857ee33f96b8aa705d307
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-26 11:00:33 +0200

    first commit

---
+++ jsnack1/main.js
@@ -0,0 +1,15 @@
+// L'utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const word1 = prompt("Inserisci una parola");
+console.log(word1);
+const word2 = prompt("Inserisci un'altra parola");
+console.log(word2);
+
+if ( word1.length > word2.length ) {
+    console.log(word1);
+} else if ( word1.length < word2.length ) {
+    console.log(word2);
+} else {
+    console.log("Le parole hanno lo stesso numero di lettere");
+}
