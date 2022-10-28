58d938202709df2e750b843b2a41b1b24779c25f
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-28 11:06:12 +0200

    Corregge consegna esercizio

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // Crea un array di numeri interi e fai la somma di tutti gli elementi 
-// che sono in posizione dispari
+// che sono in indice dispari
 
 const numbers = [ 2, 5, 6, 3, 9 ];
 
 let sum = 0;
 for (let i = 0; i < numbers.length; i++) {
-    if( i % 2 !== 0 ) {
+    if( i % 2 == 0 ) {
         console.log(numbers[i]);
         sum = sum + numbers[i];
     }
