cb7982dbcf44914da3375fff5c7778a6865bc384
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-28 10:51:43 +0200

    Aggiunge jsnack6

---
+++ jsnack6/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Crea un array di numeri interi e fai la somma di tutti gli elementi 
+// che sono in posizione dispari
+
+const numbers = [ 2, 5, 6, 3, 9 ];
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    
+    if( i % 2 === 0 ) {
+        console.log(numbers[i]);
+        sum = sum + numbers[i];
+    }
+}
+
+
